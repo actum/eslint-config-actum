84444532609bbd1f1785e8844c32d7b3c890ad02
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2016-09-07 17:15:43 +0200

    Module exports.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
 	/* Return different configuration file depending on the environment */
-	getConfig: (options = { isDev: true }) => {
+	getConfig: (options) => {
+		/* Default options */
+		options.isDev = options.isDev || true;
+
 		const configFile = options.isDev ? 'dev.js' : 'prod.js';
 		configPath = require('path').resolve(__dirname, configFile);
 
