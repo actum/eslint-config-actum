ae6b28704b0335af9746a4f3f6674733f153cdde
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2016-09-07 17:01:34 +0200

    Module exports.

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 module.exports = {
-	dev: require('./dev.js'),
-	prod: require('./prod.js')
-	
-	// getConfig: (isDev) => {
-	// 	const configPath = isDev ? './dev.js' : './prod.js';
-	// 	const config = require(configPath);
-	// 	return config;
-	// }
+	getConfig: (isDev) => {
+		const configPath = isDev ? 'dev.js' : 'prod.js';
+		configPath = __dirname + '/' + configPath;
+		return configPath;
+	}
 };
