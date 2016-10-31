1edf3b933a04d2177da96ff982e8037bbac744eb
Artem Zakharchenko <artem.zakharchenko@actum.cz>
2016-10-31 10:15:29 +0100

    Production rules

--- prod.js
+++ prod.js
@@ -2,25 +2,36 @@ module.exports = {
   extends: ['airbnb-base', 'airbnb-base/legacy'],
   plugins: ['babel', 'import'],
 
-  /* Production rules (strict) */
+  /* Production rules */
   rules: {
     /* Visual */
+    "indent": [1, 4],
+    "max-len": 0,
 
     /* Types */
     /* Referenes */
+    "no-unused-expressions": 0,
+
     /* Strings */
     /* Arrays */
     /* Objects */
+
     /* Functions */
+    "space-before-function-paren": 0,
+    "no-param-reassign": 0,
+
     /* Arrow functions */
+    "arrow-body-style": 0,
+    "no-return-assign": 0,
+
     /* Classes & Constructors */
     /* Modules */
     /*  */
-
     "no-alert": 2,
     "no-console": 2,
 
     /* Import */
-    "import/no-extraneous-dependencies": 0
+    "import/no-extraneous-dependencies": 0,
+    "import/newline-after-import": 0
   }
-}
+};
